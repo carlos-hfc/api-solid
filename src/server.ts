60c7e58d4ad7345079465c770b552c4a1720e3f5
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-09 16:42:10 -0300

    feat: add envs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { app } from "./app"
+import { env } from "./env"
 
 app
-  .listen({ port: 3333, host: "0.0.0.0" })
+  .listen({ port: env.PORT, host: "0.0.0.0" })
   .then(() => console.log("HTTP server running"))
