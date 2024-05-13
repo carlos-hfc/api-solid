612623bd97abe978603c44c0f7d0aee7c248aab1
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-13 16:23:44 -0300

    feat: add refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -29,7 +29,25 @@ export async function authenticate(
       },
     )
 
-    return reply.code(200).send({ token })
+    const refreshToken = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: "7d",
+        },
+      },
+    )
+
+    return reply
+      .setCookie("refreshToken", refreshToken, {
+        path: "/",
+        httpOnly: true,
+        secure: true,
+        sameSite: true,
+      })
+      .code(200)
+      .send({ token })
   } catch (error) {
     if (error instanceof InvalidCredentials) {
       return reply.code(400).send({
